ff8ed04431471023cf10d09930e270693a10b727
williamhyun <williamhyun3@gmail.com>
2021-05-07 14:17:35 -0700

    javadoc

--- WarOnAnts/src/EnergyPlant.java
+++ WarOnAnts/src/EnergyPlant.java
@@ -1,23 +1,24 @@
 /**
- * A version of a plant that shoots a beam of energy at any insect that is nearby. 
- * As it levels up, damage and range increase
+ * A version of a plant that shoots a beam of energy at any insect that is
+ * nearby. As it levels up, damage and range increase
+ * 
  * @author Dhruv Masurekar
- * */
+ */
 
 public class EnergyPlant extends Plant {
 
     /**
      * Initializes the EnergyPlant by giving a value to the field
      * 
-     * */ 
+     */
     public EnergyPlant() {
         super();
     }
 
     /**
-     * Shoots at ants that are in the range of this plant
-     * and deals damageDealt damage to the insect
-     * */ 
+     * Shoots at ants that are in the range of this plant and deals damageDealt
+     * damage to the insect
+     */
     public void shoot() {
 
     }
--- WarOnAnts/src/Main.java
+++ WarOnAnts/src/Main.java
@@ -1,17 +1,16 @@
 
 public class Main {
 
-	
-	DrawingSurface surface;
-	
-	public static void main(String[] args) {
-		
-		
-		System.out.println("Test");
-		
-		
-		
-		
-		
-	}
+    private DrawingSurface surface;
+
+    /**
+     * The main method.
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Test");
+
+    }
 }
