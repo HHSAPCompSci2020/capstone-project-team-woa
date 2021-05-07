6f0fcc46c0aef2797c618573d5e75b5c5673a322
derivativeman <64343507+derivativeman@users.noreply.github.com>
2021-05-07 14:01:23 -0700

    some doc comments and .jar

--- WarOnAnts/src/EnergyPlant.java
+++ WarOnAnts/src/EnergyPlant.java
@@ -1,10 +1,23 @@
+/**
+ * A version of a plant that shoots a beam of energy at any insect that is nearby. 
+ * As it levels up, damage and range increase
+ * @author Dhruv Masurekar
+ * */
 
 public class EnergyPlant extends Plant {
 
+    /**
+     * Initializes the EnergyPlant by giving a value to the field
+     * 
+     * */ 
     public EnergyPlant() {
         super();
     }
 
+    /**
+     * Shoots at ants that are in the range of this plant
+     * and deals damageDealt damage to the insect
+     * */ 
     public void shoot() {
 
     }
--- WarOnAnts/src/Plant.java
+++ WarOnAnts/src/Plant.java
@@ -1,21 +1,40 @@
+/**
+ * A version of a plant that shoots a beam of energy at any insect that is nearby. 
+ * As it levels up, damage and range increase
+ * @author Dhruv Masurekar
+ * */
 
 public class Plant {
 
-    protected int damage;
+    protected int damageDealt;
     protected int range;
 
+    /**
+     * Initializes the Plant by giving a value to the field
+     * 
+     * */ 
     public Plant() {
 
     }
 
+    /**
+     * Shoots at ants that are in the range of this plant
+     * and deals damageDealt damage
+     * */ 
     public void shoot() {
 
     }
 
+    /**
+     * The plant continues to act, which includes shooting insects that get too close 
+     * */
     public void act() {
 
     }
 
+    /**
+     * Levels up which will increase damageDealt and range
+     * */
     public void levelUp() {
 
     }
