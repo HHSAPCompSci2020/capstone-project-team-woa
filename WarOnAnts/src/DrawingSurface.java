6a2e168a9970bd2a7137fde9d97e183ef316ae4d
williamhyun <williamhyun3@gmail.com>
2021-05-11 14:13:32 -0700

    grid starter

--- WarOnAnts/src/DrawingSurface.java
+++ WarOnAnts/src/DrawingSurface.java
@@ -1,23 +1,46 @@
+
 import java.awt.Point;
 import java.awt.event.KeyEvent;
 import java.awt.geom.Point2D;
-import java.util.*;
+import java.util.ArrayList;
 
-public class DrawingSurface {
+import challenges.GridTemplate;
+import challenges.labyrinth.Labyrinth;
+import challenges.paintcan.Image;
+import challenges.maze.Maze;
+import processing.core.PApplet;
 
+public class DrawingSurface extends PApplet {
+
+    // When you progress to a new prompt, modify this field.
     private Grid grid;
 
-    /**
-     * Sets grid to a new GridTemplate.
-     */
     public DrawingSurface() {
-
+        grid = new Grid("testfiles/labyrinth/test2.txt");
+        System.out.println(grid);
     }
 
-    /**
-     * Updates the graphics
-     */
     public void draw() {
+        background(255);
+        fill(0);
+        textAlign(LEFT);
+        textSize(12);
+
+        if (grid != null) {
+            grid.draw(this, 0, 0, height, height);
+        }
 
     }
+
+    public void mousePressed() {
+        if (mouseButton == LEFT) {
+            Point click = new Point(mouseX, mouseY);
+            float dimension = height;
+            Point cellCoord = grid.clickToIndex(click, 0, 0, dimension, dimension);
+            if (cellCoord != null) {
+                // function
+            }
+        }
+    }
+
 }
